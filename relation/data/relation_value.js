da5bcb0dc454f92d4d65c5eea865beefa57cd715
fortyMiles <mqgao@outlook.com>
2015-12-22 18:13:09 +0800

    could caculate any two pair people in home relation

--- relation/data/relation_value.js
+++ relation/data/relation_value.js
@@ -9,29 +9,77 @@
 var _ = require('ramda');
 
 var VALUE = {
-	'妻子': {weight: 1, level:1, ctf: '丈夫', ctm: '丈夫'},
-	'丈夫': {weight: 1, level:1, ctf: '妻子', ctm: '妻子'},
-	'儿子': {weight: 2, level:1, ctf: '母亲', ctm: '父亲'},
-	'女儿': {weight: 2, level:1, ctf: '母亲', ctm: '父亲'},
-	'弟弟': {weight: 3, level:1, ctf: '姐姐', ctm: '哥哥'},
-	'哥哥': {weight: 3, level:1, ctf: '妹妹', ctm: '弟弟'},
-	'妹妹': {weight: 3, level:1, ctf: '姐姐', ctm: '哥哥'},
-	'姐姐': {weight: 3, level:1, ctf: '妹妹', ctm: '弟弟'},
-	'父亲': {weight: 3, level:1, ctf: '女儿', ctm: '儿子'},
-	'母亲': {weight: 3, level:1, ctf: '女儿', ctm: '儿子'},
-	'孙子': {weight: 4, level:2, ctf: '奶奶', ctm: '爷爷'},
-	'孙女': {weight: 4, level:2, ctf: '奶奶', ctm: '爷爷'},
-	'女婿': {weight: 4, level:2, ctf: '岳母', ctm: '岳父'},
-	'媳妇': {weight: 4, level:2, ctf: '婆婆', ctm: '公公'},
-	'爷爷': {weight: 5, level:2, ctf: '孙女', ctm: '孙子'},
-	'奶奶': {weight: 5, level:2, ctf: '孙女', ctm: '孙子'},
-	'外公': {weight: 5, level:2, ctf: '外孙女', ctm: '外孙'},
-	'外婆': {weight: 5, level:2, ctf: '外孙女', ctm: '外孙'},
-	'岳父': {weight: 5, level:2, ctf: '女婿', ctm: '女婿'},
-	'岳母': {weight: 5, level:2, ctf: '女婿', ctm: '女婿'},
-	'公公': {weight: 5, level:2, ctf: '媳妇', ctm: '媳妇'},
-	'婆婆': {weight: 5, level:2, ctf: '媳妇', ctm: '媳妇'},
-	'亲家': {weight: 6, level:3, ctf: '亲家', ctm: '亲家'},
+	'自己': {code: 0, weight: 0, level:0, ctf: '自己', ctm: '自己'},
+	'妻子': {code: 1, weight: 1, level:1, ctf: '丈夫', ctm: '丈夫'},
+	'丈夫': {code: 2, weight: 1, level:1, ctf: '妻子', ctm: '妻子'},
+	'儿子': {code: 3, weight: 2, level:1, ctf: '母亲', ctm: '父亲'},
+	'女儿': {code: 4, weight: 2, level:1, ctf: '母亲', ctm: '父亲'},
+	'弟弟': {code: 5, weight: 3, level:1, ctf: '姐姐', ctm: '哥哥'},
+	'哥哥': {code: 6, weight: 3, level:1, ctf: '妹妹', ctm: '弟弟'},
+	'妹妹': {code: 7, weight: 3, level:1, ctf: '姐姐', ctm: '哥哥'},
+	'姐姐': {code: 8, weight: 3, level:1, ctf: '妹妹', ctm: '弟弟'},
+	'父亲': {code: 9, weight: 3, level:1, ctf: '女儿', ctm: '儿子'},
+	'母亲': {code: 10, weight: 3, level:1, ctf: '女儿', ctm: '儿子'},
+	'孙子': {code: 11, weight: 4, level:2, ctf: '奶奶', ctm: '爷爷'},
+	'孙女': {code: 12, weight: 4, level:2, ctf: '奶奶', ctm: '爷爷'},
+	'外孙女': {code: 13, weight: 4, level:2, ctf: '外婆', ctm: '外公'},
+	'外孙': {code: 14, weight: 4, level:2, ctf: '外婆', ctm: '外公'},
+	'女婿': {code: 15, weight: 4, level:2, ctf: '岳母', ctm: '岳父'},
+	'儿媳': {code: 16, weight: 4, level:2, ctf: '婆婆', ctm: '公公'},
+	'爷爷': {code: 17, weight: 5, level:2, ctf: '孙女', ctm: '孙子'},
+	'奶奶': {code: 18, weight: 5, level:2, ctf: '孙女', ctm: '孙子'},
+	'外公': {code: 19, weight: 5, level:2, ctf: '外孙女', ctm: '外孙'},
+	'外婆': {code: 20, weight: 5, level:2, ctf: '外孙女', ctm: '外孙'},
+	'岳父': {code: 21, weight: 5, level:2, ctf: '女婿', ctm: '女婿'},
+	'岳母': {code: 22, weight: 5, level:2, ctf: '女婿', ctm: '女婿'},
+	'公公': {code: 23, weight: 5, level:2, ctf: '儿媳', ctm: '儿媳'},
+	'婆婆': {code: 24, weight: 5, level:2, ctf: '儿媳', ctm: '儿媳'},
+	'亲家': {code: 25, weight: 6, level:3, ctf: '亲家', ctm: '亲家'},
+};
+
+var TITLES = [];
+
+for(var title in VALUE){
+	TITLES.push(title);
+}
+
+/*
+ * Define the different position how to call another position.
+ *
+ */
+
+var relationMaxtix = {
+	1:{0: 2},
+	2:{0: 25, 1: 25},
+	3:{0: 25, 1: 25, 2: 2},
+	4:{0: 9,  1: 10, 2: 21, 3: 22},
+	5:{0: 23, 1: 24, 2: 9,  3: 10, 4: 2},
+	6:{0: 17, 1: 18, 2: 19, 3: 20, 4: 9, 5:10},
+};
+
+/*
+ * Get title.
+ *
+ * @param {String} start position
+ * @param {String} end position
+ * @param {Boolean} end if is male. if A is an old man, B is a young person, when B is male, the title resutle could be 孙子, but when B is famle, result could be 孙女.
+ *
+ */
+
+var get_title = function(start, end, end_is_male){
+	var relation_code = null;
+	if(start > end){
+		relation_code = relationMaxtix[start][end];
+		relation = TITLES[relation_code];
+	}else{
+		// get converse relation.
+		converse_relation_code = relationMaxtix[end][start]; 
+		converse_relation = TITLES[converse_relation_code]; // get 爷爷
+		if((converse_relation in VALUE) === false){debugger;};
+		relation = get_converse_relation(end_is_male, converse_relation); // get 孙女 or 孙子
+	}
+	
+	return relation;
 };
 
 /*
@@ -83,4 +131,5 @@ var relation_accept = _.curry(key_in_dic)(VALUE);
 module.exports = {
 	get_converse_relation: get_converse_relation,
 	relation_accept: relation_accept,
+	get_title: get_title,
 };
--- relation/data/tests.js
+++ relation/data/tests.js
@@ -13,8 +13,8 @@ var chai = require('chai');
 
 var assert = chai.assert;
 
-describe('Get converse relation', function(){
-	describe('#get_converse_relation', function(){
+describe('Relation Caculation', function(){
+	describe('#get_converse_relation()', function(){
 		it('should relation 丈夫 if A calls B 妻子, and A is male', function(){
 			var is_male = true;
 			var relation = '妻子';
@@ -39,6 +39,111 @@ describe('Get converse relation', function(){
 			var converse = '孙女';
 			assert.equal(R.get_converse_relation(is_male, relation), converse);
 		});
+	});
+
+	describe('#get_title()', function(){
+
+		var test_data = [
+			[true, 1, 0, '丈夫'],
+
+			[true, 2, 0, '亲家'],
+			[true, 2, 1, '亲家'],
+
+			[true, 3, 0, '亲家'],
+			[true, 3, 1, '亲家'],
+			[true, 3, 2, '丈夫'],
+
+			[true, 4, 0, '父亲'],
+			[true, 4, 1, '母亲'],
+			[true, 4, 2, '岳父'],
+			[true, 4, 3, '岳母'],
+
+			[true, 5, 0, '公公'],
+			[true, 5, 1, '婆婆'],
+			[true, 5, 2, '父亲'],
+			[true, 5, 3, '母亲'],
+			[true, 5, 4, '丈夫'],
+
+			[true, 6, 0, '爷爷'],
+			[true, 6, 1, '奶奶'],
+			[true, 6, 2, '外公'],
+			[true, 6, 3, '外婆'],
+			[true, 6, 4, '父亲'],
+			[true, 6, 5, '母亲'],
+
+			[false, 0, 1, '妻子'],
+			[false, 0, 2, '亲家'],
+			[false, 0, 3, '亲家'],
+			[false, 0, 4, '女儿'],
+			[false, 0, 5, '儿媳'],
+			[false, 0, 6, '孙女'],
+
+			[false, 1, 2, '亲家'],
+			[false, 1, 3, '亲家'],
+			[false, 1, 4, '女儿'],
+			[false, 1, 5, '儿媳'],
+			[false, 1, 6, '孙女'],
+
+			[false, 2, 3, '妻子'],
+			[false, 2, 4, '女婿'],
+			[false, 2, 5, '女儿'],
+			[false, 2, 6, '外孙女'],
+
+			[false, 3, 4, '女婿'],
+			[false, 3, 5, '女儿'],
+			[false, 3, 6, '外孙女'],
+
+			[false, 4, 5, '妻子'],
+			[false, 4, 6, '女儿'],
+
+			[false, 5, 6, '女儿'],
+
+
+			[true, 0, 1, '妻子'],
+			[true, 0, 2, '亲家'],
+			[true, 0, 3, '亲家'],
+			[true, 0, 4, '儿子'],
+			[true, 0, 5, '儿媳'],
+			[true, 0, 6, '孙子'],
+
+			[true, 1, 2, '亲家'],
+			[true, 1, 3, '亲家'],
+			[true, 1, 4, '儿子'],
+			[true, 1, 5, '儿媳'],
+			[true, 1, 6, '孙子'],
+
+			[true, 2, 3, '妻子'],
+			[true, 2, 4, '女婿'],
+			[true, 2, 5, '儿子'],
+			[true, 2, 6, '外孙'],
+
+			[true, 3, 4, '女婿'],
+			[true, 3, 5, '儿子'],
+			[true, 3, 6, '外孙'],
+
+			[true, 4, 5, '妻子'],
+			[true, 4, 6, '儿子'],
+
+			[true, 5, 6, '儿子'],
+
+		];
+
+
+			test_data.map(function(entry){
+
+				var end_is_male = entry[0];
+				var start = entry[1];
+				var end = entry[2];
+				var expected = entry[3];
+
+				it('should return ' + expected +' when (start, end) == ('+ start +','+ end +')', function(){
+
+				var relation = R.get_title(start, end, end_is_male);
+
+				assert.equal(relation, expected);
+			});
+		});
+
 	});
 });
 
